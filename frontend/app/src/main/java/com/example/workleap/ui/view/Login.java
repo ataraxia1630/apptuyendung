e777eac3ea8c5f0f6d4640b4d78cb55488addd93
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-04-03 22:43:39 +0700

    📂 Created basic activities

---
+++ frontend/app/src/main/java/com/example/workleap/ui/view/Login.java
@@ -0,0 +1,51 @@
+package com.example.workleap.ui.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.workleap.R;
+import com.example.workleap.ui.viewmodel.UserViewModel;
+
+public class Login extends AppCompatActivity {
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private UserViewModel userViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etEmail = findViewById(R.id.email_edittext);
+        etPassword = findViewById(R.id.password_edittext);
+        btnLogin = findViewById(R.id.login_button);
+
+        btnLogin.setOnClickListener(v -> loginWithEmail());
+
+    }
+    private void loginWithEmail() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //userViewModel.login(new User(email, password, null));
+    }
+}
---
+++ frontend/app/src/main/java/com/example/workleap/ui/view/Signup.java
@@ -0,0 +1,32 @@
+package com.example.workleap.ui.view;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.workleap.R;
+import com.example.workleap.ui.viewmodel.UserViewModel;
+
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
