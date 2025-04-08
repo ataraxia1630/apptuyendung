f21aa26c3efa93623c9ac1a4e526ad86acf4cf3e
nhittyn <hudinhdong@gmail.com>
2025-04-08 10:50:20 +0700

    skill api (ongoing)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,13 @@
 const authRoutes = require('./auth.route');
 const applicantRoutes = require('./applicant.route');
 
+const applicantSkillRoutes = require('./applicantSkill.route');
+
 function route(app) {
   app.use('/api/auth', authRoutes);
   app.use('/api/users/applicant', applicantRoutes);
+
+  app.use('/api/skill', applicantSkillRoutes);
 }
 
 module.exports = route;
