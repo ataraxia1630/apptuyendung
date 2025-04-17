0c72c65ea8f940b72f35e71660915f4aacc5232b
nhittyn <hudinhdong@gmail.com>
2025-04-18 04:59:14 +0700

    applicant valid

--- backend/src/routes/applicant.route.js
+++ backend/src/routes/applicant.route.js
@@ -2,6 +2,10 @@ const { Router } = require('express');
 const { ApplicantController } = require('../controllers/applicant.controller');
 
 const { verifyToken } = require('../middlewares/auth.middleware');
+const { validate } = require('../middlewares/validate.middleware');
+const {
+  ApplicantSchema,
+} = require('../validators/Applicant/updateApplicant.validator');
 
 const route = Router();
 
@@ -17,7 +21,14 @@ route.get(
 
 // lấy 1 người dùng theo id / username
 route.get('/:id', verifyToken, ApplicantController.getApplicantById);
-route.put('/:id', verifyToken, ApplicantController.updateApplicant);
+route.put(
+  '/:id',
+  verifyToken,
+  validate(ApplicantSchema),
+  ApplicantController.updateApplicant
+);
+
+// xóa người dùng theo id
 route.delete('/:id', verifyToken, ApplicantController.deleteApplicant);
 
 module.exports = route;
