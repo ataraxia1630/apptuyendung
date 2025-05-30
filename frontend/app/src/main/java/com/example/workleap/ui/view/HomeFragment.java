b3df3d59bca369b9383ff4cab252c794b36fbda3
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-05-30 23:54:45 +0700

    Load jobpost on home page

--- frontend/app/src/main/java/com/example/workleap/ui/view/HomeFragment.java
+++ frontend/app/src/main/java/com/example/workleap/ui/view/HomeFragment.java
@@ -2,13 +2,24 @@ package com.example.workleap.ui.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.workleap.R;
+import com.example.workleap.data.model.entity.JobPost;
+import com.example.workleap.ui.viewmodel.JobPostViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,44 +28,21 @@ import com.example.workleap.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private JobPostAdapter adapter;
+    private List<JobPost> allJobs = new ArrayList<>();
+    private JobPostViewModel jobPostViewModel;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,4 +51,30 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerJobPosts); // ID trong layout
+        jobPostViewModel = new ViewModelProvider(requireActivity()).get(JobPostViewModel.class);
+        jobPostViewModel.InitiateRepository(getContext());
+
+
+        jobPostViewModel.getAllJobPostResult().observe(getViewLifecycleOwner(), result ->
+        {
+            String s = result.toString();
+            Log.e("HomeFragment", "getAllJobPostResult: " + s + "");
+        });
+        jobPostViewModel.getAllJobPostData().observe(getViewLifecycleOwner(), jobPosts ->
+        {
+            allJobs.addAll(jobPosts);
+            // Setup RecyclerView
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            adapter = new JobPostAdapter(allJobs); // mặc định show tất cả
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        });
+        jobPostViewModel.getAllJobPosts();
+    }
+
 }
