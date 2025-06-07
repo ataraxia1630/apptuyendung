8934f7b6a77cee3994c62756e3e7233ec8597998
nhittyn <hudinhdong@gmail.com>
2025-06-07 12:47:35 +0700

    notification for job applied

--- backend/src/emitters/notification.emitter.js
+++ backend/src/emitters/notification.emitter.js
@@ -3,13 +3,31 @@ const { NotiEventHandler } = require('../socket/noti.eventHandler');
 
 const NotiEmitter = new EventEmitter();
 
-NotiEmitter.on('job.applied', async ({ userId, jobTitle }) => {
-  await NotiEventHandler.notify({
+NotiEmitter.on('job.apply', async ({ userId, jobTitle }) => {
+  await NotiEventHandler.notify(
     userId,
-    title: 'Có ứng viên vừa ứng tuyển!',
-    message: `Có ứng viên mới ứng tuyển vào công việc: ${jobTitle}`,
-    type: 'both',
-  });
+    'Có ứng viên vừa ứng tuyển!',
+    `Có ứng viên mới ứng tuyển vào công việc: ${jobTitle}`,
+    'both'
+  );
 });
 
-module.exports = new NotiEmitter();
+NotiEmitter.on('jobApplied.success', async ({ userId, jobTitle }) => {
+  await NotiEventHandler.notify(
+    userId,
+    'Ứng tuyển thành công!',
+    `Chúc mừng bạn! Bạn đã ứng tuyển thành công vào công việc: ${jobTitle}`,
+    'both'
+  );
+});
+
+NotiEmitter.on('jobApplied.fail', async ({ userId, jobTitle }) => {
+  await NotiEventHandler.notify(
+    userId,
+    'Ứng tuyển thất bại!',
+    `Rất tiếc, bạn đã ứng tuyển thất bại vào công việc: ${jobTitle}`,
+    'both'
+  );
+});
+
+module.exports = NotiEmitter;
