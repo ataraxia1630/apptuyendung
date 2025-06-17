aee697d89a306d85bf3f40028cddadc8f35665c1
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-06-17 13:29:28 +0700

    implement statistic library

--- frontend/app/src/main/java/com/example/workleap/ui/view/main/StatisticsFragment.java
+++ frontend/app/src/main/java/com/example/workleap/ui/view/main/StatisticsFragment.java
@@ -1,7 +1,9 @@
 package com.example.workleap.ui.view.main;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +11,88 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.workleap.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatisticsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    PieChart pieChart;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StatisticsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatisticsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatisticsFragment newInstance(String param1, String param2) {
-        StatisticsFragment fragment = new StatisticsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    LineChart lineChart;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        pieChart = view.findViewById(R.id.pieChart);
+
+        setupPieChart();
+        loadPieChartData();
+
+        lineChart = view.findViewById(R.id.lineChart);
+        setupLineChart();
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+    private void setupPieChart() {
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
     }
+    private void setupLineChart() {
+        // Dữ liệu mẫu (VD: số CV theo từng ngày)
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 5));   // Ngày 1: 5 CV
+        entries.add(new Entry(2, 9));   // Ngày 2: 9 CV
+        entries.add(new Entry(3, 6));   // Ngày 3: 6 CV
+
+        LineDataSet dataSet = new LineDataSet(entries, "Số lượng CV mỗi ngày");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setCircleColor(Color.RED);
+        dataSet.setLineWidth(2f);
+        dataSet.setValueTextSize(12f);
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+
+        // Tuỳ chỉnh trục, hiệu ứng
+        lineChart.getDescription().setText("Thống kê CV theo ngày");
+        lineChart.animateX(1000);
+        lineChart.invalidate(); // refresh
+    }
+
+    private void loadPieChartData() {
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(40f, "Đã duyệt"));
+        entries.add(new PieEntry(30f, "Chờ duyệt"));
+        entries.add(new PieEntry(30f, "Từ chối"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Trạng thái hồ sơ");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(15f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate(); // refresh
+    }
+
 }
