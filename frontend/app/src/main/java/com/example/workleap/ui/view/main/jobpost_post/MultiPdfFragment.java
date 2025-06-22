5f252a746b784b223283e39049461e2b83c944df
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-06-22 19:52:44 +0700

    set status cv in reviewing fragment

--- frontend/app/src/main/java/com/example/workleap/ui/view/main/jobpost_post/MultiPdfFragment.java
+++ frontend/app/src/main/java/com/example/workleap/ui/view/main/jobpost_post/MultiPdfFragment.java
@@ -1,21 +1,25 @@
 package com.example.workleap.ui.view.main.jobpost_post;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.workleap.R;
 import com.example.workleap.data.model.entity.JobApplied;
 import com.example.workleap.data.model.entity.JobPost;
 import com.example.workleap.ui.view.main.cv_appliedjob.PdfFragment;
+import com.example.workleap.ui.viewmodel.JobPostViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +30,14 @@ public class MultiPdfFragment extends Fragment {
     private ArrayList<String> applicantNames;
     private int currentIndex = 0;
 
-    private TextView tvApplicantName, tvPdfIndex;
+    private TextView tvApplicantName, tvPdfIndex, tvStatus;
     private ImageButton btnPrevious, btnNext, btnDismiss, btnApprove, btnBack;
 
     private ArrayList<JobApplied> jobApplieds;
+
+    private JobApplied currentJobApplied;
+
+    private JobPostViewModel jobPostViewModel;
 
     private String urlSupabase = "https://epuxazakjgtmjuhuwkza.supabase.co/storage/v1/object/public/cv-storage/";
 
@@ -62,8 +70,24 @@ public class MultiPdfFragment extends Fragment {
             applicantNames.add(item.getApplicant().getFirstName() +" "+ item.getApplicant().getLastName());
         }
 
+        jobPostViewModel = new ViewModelProvider(requireActivity()).get(JobPostViewModel.class);
+        jobPostViewModel.getProcessCvAppliedData().observe(getViewLifecycleOwner(), jobApplied -> {
+            if(jobApplied==null)
+            {
+                Toast.makeText(getContext(), "An error occurred while updating the CV evaluation result. Please try again later.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getContext(), "CV review updated successfully.", Toast.LENGTH_SHORT).show();
+                tvStatus.setText(jobApplied.getStatus());
+                //cap nhat cho jobapplied trong danh sach
+                jobApplieds.get(currentIndex).setStatus(jobApplied.getStatus());
+                SetColorForStatus();
+            }
+        });
+
         tvApplicantName = view.findViewById(R.id.tvApplicantName);
         tvPdfIndex = view.findViewById(R.id.tvPdfIndex);
+        tvStatus = view.findViewById(R.id.tvStatus);
         btnBack = view.findViewById(R.id.btnBack);
         btnPrevious = view.findViewById(R.id.btnPrevious);
         btnNext = view.findViewById(R.id.btnNext);
@@ -83,16 +107,20 @@ public class MultiPdfFragment extends Fragment {
         if (pdfUrls == null || index < 0 || index >= pdfUrls.size()) return;
         currentIndex = index;
 
+        currentJobApplied = jobApplieds.get(index);
         String url = pdfUrls.get(currentIndex);
         String applicant = applicantNames.get(currentIndex);
 
         tvApplicantName.setText(applicant);
         tvPdfIndex.setText((currentIndex + 1) + "/" + pdfUrls.size());
+        tvStatus.setText(currentJobApplied.getStatus());
+        SetColorForStatus();
 
         PdfFragment pdfFragment = new PdfFragment();
         Bundle args = new Bundle();
         args.putString("pdf_url", urlSupabase + url);
         args.putString("pdf_title", applicant);
+        args.putBoolean("hide_toolbar", true);
         pdfFragment.setArguments(args);
 
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
@@ -101,10 +129,19 @@ public class MultiPdfFragment extends Fragment {
     }
 
     private void handleDismiss() {
-        // TODO: xử lý logic loại bỏ hồ sơ
+        jobPostViewModel.processCvApplied(currentJobApplied.getJobpostId(), currentJobApplied.getApplicantId(), "FAILURE");
     }
 
     private void handleApprove() {
-        // TODO: xử lý logic duyệt hồ sơ
+        jobPostViewModel.processCvApplied(currentJobApplied.getJobpostId(), currentJobApplied.getApplicantId(), "SUCCESS");
+    }
+
+    private void SetColorForStatus()
+    {
+        if(String.valueOf(tvStatus.getText()).equalsIgnoreCase("SUCCESS"))
+            tvStatus.setTextColor(Color.GREEN);
+        if(String.valueOf(tvStatus.getText()).equalsIgnoreCase("FAILURE"))
+            tvStatus.setTextColor(Color.RED);
     }
 }
+
